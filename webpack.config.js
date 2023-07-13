0d50f5962d9f8676a396f937eecfd2a565baf6ac
MAGHC <windowsapple95@gmail.com>
2023-07-13 17:19:50 +0900

    feat : asset dataUrlcondition magic number 변경

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
   mode: 'none',
   module: {
     rules: [
-      { test: /\.(png|jpg)$/, type: 'asset/inline' },
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, //4kb 넘으면 asset/resource
+          },
+        },
+      },
       {
         test: /\.(ttf)$/,
         type: 'asset/resource',
