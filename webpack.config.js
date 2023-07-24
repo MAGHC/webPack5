5458fae5cdf00efd638c476c5d5fb0acdee34414
MAGHC <windowsapple95@gmail.com>
2023-07-24 19:37:13 +0900

    feat: plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -31,6 +33,18 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
     ],
   },
+  plugins: [new TerserPlugin()],
 };
