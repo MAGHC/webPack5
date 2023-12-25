f04c1fbe3e48bbedd92fad2f25d53ad29f1c26c5
MAGHC <windowsapple95@gmail.com>
2023-12-25 21:11:34 +0900

    웹팩 다른걸로 복습~import 까지

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("우리집"),console.log(((o=21)=>crypto.getRandomValues(new Uint8Array(o)).reduce(((o,e)=>o+((e&=63)<36?e.toString(36):e<62?(e-26).toString(36).toUpperCase():e>62?"-":"_")),""))())})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import getRandomAddress from './randomAddress';
+import { nanoid } from 'nanoid';
+
+console.log(getRandomAddress());
+console.log(nanoid());
